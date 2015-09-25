99931d37645ca1dde60c5c1969fb937562142f81
Ben Gourarie <bgourarie@uptopcorp.com>
2015-09-25 13:42:46 -0700

    changing text for shipping address label

--- themes/the-dream-bed/woocommerce/checkout/form-shipping.php
+++ themes/the-dream-bed/woocommerce/checkout/form-shipping.php
@@ -31,7 +31,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<h3 id="ship-to-different-address">
 			<label for="ship-to-different-address-checkbox" class="checkbox"><?php _e( 'Ship to a different address?', 'woocommerce' ); ?></label>
 			<input id="ship-to-different-address-checkbox" class="input-checkbox" <?php checked( $ship_to_different_address, 1 ); ?> type="checkbox" name="ship_to_different_address" value="1" />
-			<span>Use My Billing Address</span>
+			<span>Ship to a different address</span>
 		</h3>
 
 		<div class="shipping_address">
