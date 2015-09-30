6da87abca5a77f325d3a7b799eb7850cbf6de0a8
Rhia Bucklin <rbucklin@uptopcorp.com>
2015-09-30 12:01:45 -0700

    added favicon

--- themes/the-dream-bed/header.php
+++ themes/the-dream-bed/header.php
@@ -13,7 +13,8 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<link rel="icon" href="<?php bloginfo("template_url"); ?>/dream-favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="<?php bloginfo("template_url"); ?>/dream-favicon.ico" type="image/x-icon">
 <?php wp_head(); ?>
 
 
