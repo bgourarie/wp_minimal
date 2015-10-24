76a70d0e28577f824828138557ddab053fe857b4
Ben Gourarie <ben.gourarie@gmail.com>
2015-10-24 13:35:23 -0700

    minor changes to menu to get things to stay in the right place when needed

--- themes/wp_minimal_bg/header.php
+++ themes/wp_minimal_bg/header.php
@@ -44,16 +44,17 @@
 
 	<nav class="navbar navbar-default">
 	  <div class="container-fluid ryan-menu">
-	    <div class="navbar-header ryan-menu-padding">
+	    <div class="navbar-left ryan-menu-left">
+	      <a class="navbar-logo" href="#">
+	      	<img class="navbar-logo-png" src="<?php echo get_bloginfo('template_directory'); ?>/library/images/rcain-logo.jpg"/>
+	      </a>
 	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
 	        <span class="sr-only">Toggle navigation</span>
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	      </button>
-	      <a class="navbar-logo" href="#">
-	      	<img class="navbar-logo-png" src="<?php echo get_bloginfo('template_directory'); ?>/library/images/rcain-logo.jpg"/>
-	      </a>
+	      
 	    </div>
     <div class="collapse navbar-collapse" id="navbar-collapse">
       <ul class="nav navbar-nav navbar-right">
