dbfbef6334f4d28c5b7dc356e2a94ac430d3c3d0
Ben Gourarie <bgourarie@uptopcorp.com>
2015-11-07 13:49:52 -0800

    changing gallery-password to just use a conditional

--- themes/wp_minimal_bg/page-gallery-password.php
+++ themes/wp_minimal_bg/page-gallery-password.php
@@ -16,15 +16,20 @@
 <?php get_header(); ?>
 
 			<div id="content">
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+							<?php if (have_posts()) : while (have_posts()) : the_post(); 
+
+							if(is_password_required($post)){
+								echo get_the_password_form();
+							}
+
+
+							else{?>
+
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
 
-								<section class="entry-content cf" itemprop="articleBody">
-									<?php
-										// the content (pretty self explanatory huh)
-										the_content();
+<?php									
 				
 						$args = array(
 						    'numberposts' => -1, // Using -1 loads all posts
@@ -43,39 +48,8 @@
 							    <?php    } ?>
 						  </div>
 						<?php }
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-									?>
-								</section>
-
-
-								<footer class="article-footer">
-
-                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-								</footer>
-
-								<?php comments_template(); ?>
-
-							</article>
-
-							<?php endwhile; else : ?>
+								}
+						<?php endwhile; else : ?>
 
 									<article id="post-not-found" class="hentry cf">
 											<header class="article-header">
@@ -92,8 +66,6 @@
 							<?php endif; ?>
 
 						</main>
-
-						<?php get_sidebar(); ?>
 
 				</div>
 
