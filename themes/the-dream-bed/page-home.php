a66b97faad4aaea7b7ba4201a91f80b394b64eaa
Rhia Bucklin <rbucklin@uptopcorp.com>
2015-11-12 12:24:34 -0800

    removed extraneous content on home page to create an easier flow to the two product pages

--- themes/the-dream-bed/page-home.php
+++ themes/the-dream-bed/page-home.php
@@ -56,7 +56,7 @@ jQuery(document).ready(function($){
 
 <?php } ?>
 
-<div class="container">
+<div class="container hidden-xs">
 	<div class="special-circles">
 		<div class="row">
 			<div class="col-sm-3 col-xs-6 text-center">
@@ -107,8 +107,16 @@ jQuery(document).ready(function($){
 			<div class="col-sm-12 center-block text-center home-bed-options">
 				<h2>The Dream Bed comes in two dreamy choices</h2>
 				<p><img src="<?php bloginfo("template_url"); ?>/images/photos/two-dreamy-options-man.jpg" alt="A gentleman demonstrates the difficulty of choosing between the two Dream Bed options." class="img-responsive center-block"></p>
-				<div class="center-block"> 
+				<div class="center-block hidden-xs"> 
 					<a href="<?php bloginfo('url'); ?>/shop/" class="btn btn-dream" role="button">Shop The Dream Bed</a>
+				</div>
+				<div class="center-block visible-xs">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-6"><a href="<?php bloginfo('url'); ?>/shop/mattress/dream-bed/" class="btn btn-dream btn-block" role="button">Shop Original Dream</a></div>
+							<div class="col-xs-6"><a href="<?php bloginfo('url'); ?>/shop/mattress/cool-gel-bed/" class="btn btn-dream btn-block" role="button">Shop Cool Dream</a></div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
