642ee813373b16b7e03308dfec48e208aa2e7ccd
Ben Gourarie <bgourarie@uptopcorp.com>
2016-01-06 09:31:41 -0800

    adding in blog stuff
    changes to blog page and post pages to fix errors
    
    changes to use ID in fetching content rather than the loop
    
    more fixes
    
    small changes to the blog posts

---
+++ themes/the-dream-bed/page-blog.php
@@ -0,0 +1,102 @@
+<?php get_header(); 
+
+// then we start with site title and search box:
+$blogpage = get_page_by_title('Blog');
+
+?>
+<div class="blog-header">
+	<a href="<?php get_page_link($blogpage->ID);?>">
+		<div class="blog-title">
+			<?php echo get_field('blog_title');?>
+		</div>
+	</a>
+	<div class="blog-search">
+		<?php get_search_form( true ); ?>
+	</div>
+	<div class="blog-categories">
+		little colorful line will go here
+	</div>
+</div>
+
+<?php 
+	// get the posts, but for this, restrict to posts with the "featured" meta value -- that's the featured post. 
+	$posts = get_posts(array('meta_key'=>'featured','meta_value'=>1));
+	foreach($posts as $feature){
+		setup_postdata($feature);
+		$categories = get_the_category();
+		$featured_id = $feature->ID;
+?>
+<div class="blog-featured">
+	<a href="<?php echo get_permalink(); ?>" title="<?php echo get_the_title($featured_id); ?>">
+		<div class="blog-featured-hero">
+		 <?php echo get_the_post_thumbnail( $featured_id, "large" ); ?> 
+		</div>
+		<div class="blog-feature-title">
+			<?php  echo get_the_title($featured_id); ?> 
+		</div>
+		<div class="blog-feature-excerpt">
+			<?php echo get_the_excerpt($featured_id);?>
+		</div>
+	</a>
+	<?php if ( ! empty( $categories ) ) { ?>
+		<a href="<?php echo get_category_link( $categories[0]->ID); ?>"  class="<?php echo esc_html( $categories[0]->name );?>">
+		</a>
+	<?php 	}  ?>
+</div>
+<?php wp_reset_postdata();
+
+} 
+?>
+
+<div class="blog-other-posts">
+	<div class="blog-other-header">
+		See what else is new...
+	</div>
+
+	<?php
+	// get all posts in reverse chronological order, 16 per page. Exclude the featured one. 
+	$args = array(
+		'posts_per_page'   => 16,
+		'offset'           => 0,
+		'orderby'          => 'date',
+		'order'            => 'DESC',
+		'exclude'          => $featured_id,
+		'post_type'        => 'post',
+		'post_status'      => 'publish',
+		'suppress_filters' => true 
+	);
+	$posts = get_posts($args);
+	foreach($posts as $post){
+		setup_postdata($post);
+		$categories = get_the_category();	?>
+
+		<div class="blog-other-teaser">
+			<a href="<?php echo get_permalink($post->ID); ?>" title="<?php get_the_title($post->ID); ?>">
+				<?php //see https://developer.wordpress.org/reference/functions/get_the_post_thumbnail/#comment-314 
+					if(has_post_thumbnail($post->ID)){
+						echo get_the_post_thumbnail($post->ID,'medium');
+					}else{
+						// put in a default/placeholder blog image?
+					}
+				?>
+				<div class="blog-other-title">
+					<?php echo get_the_title( $post->ID );?> >
+				</div>
+				<div class="blog-other-excerpt">
+					<?php echo get_the_excerpt($post->ID); ?>
+				</div>
+			</a>
+			<?php if ( ! empty( $categories ) ) {?>
+				<a href="<?php echo get_category_link( $categories[0]->ID); ?>"  class="<?php echo esc_html( $categories[0]->name );?>">
+				</a>
+			<?php 	}  ?>
+		</div>
+	<?php wp_reset_postdata($post);
+	}
+	?>
+	<a href="load-more">
+		Load More
+	</a>
+</div>
+
+<?php get_footer(); ?>
--- themes/the-dream-bed/single.php
+++ themes/the-dream-bed/single.php
@@ -11,10 +11,33 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
+				<div class="single post have posts">
+				</div>
+			<?php //get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			<?php //the_post_navigation(); ?>
+			<?php	$categories = get_the_category();?>
+			<?php 
+			if ( ! empty( $categories ) ) { ?>
+				<a href="<?php echo get_category_link( $categories[0]->ID); ?>"  class="<?php echo esc_html( $categories[0]->name );?>">
+				</a>
+			<?php 	}  ?>
+			<?php // echo the title... can move the div out of the method call 
+			<?php  echo get_the_title(get_the_ID()); ?> 
 
-			<?php the_post_navigation(); ?>
+			<?php 
+			// this does all the content. embedded images are reliant on the styling within the post.
+			// a container can be put around this if needed. (probably a good idea...)
+			echo get_the_content(); ?>
+
+			<?php // there's a way to write a widget.. i'll find it out. and destroy it. 
+			//post_sharing_widget(get_the_ID()); ?>
+
+			<?php 
+			// need to write this, put it into incldues and ensure it gets include..d... ?
+
+				//get_related_posts(get_the_ID());
+			?>
 
 
 		<?php endwhile; // End of the loop. ?>
