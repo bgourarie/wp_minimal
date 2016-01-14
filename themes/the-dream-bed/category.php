98441dff9a61c8e7d4c44569a42f84ba541f148e
Ben Gourarie <bgourarie@uptopcorp.com>
2016-01-14 11:22:06 -0800

    updating category page to match the regular blog page

--- themes/the-dream-bed/category.php
+++ themes/the-dream-bed/category.php
@@ -2,51 +2,85 @@
 /**
 * A Simple Category Template
 */
-
+ 
 get_header(); 
 get_blog_header(); 
+
 ?> 
 
 <section id="primary" class="site-content">
-<div id="content" role="main">
+<div class="blog-other-posts">
+	<div class="container">
+		<div class="row">
 
-<?php 
-// Check if there are any posts to display
-if ( have_posts() ) : ?>
+	<?php
+	// get all posts for current category
+	$cat = get_category( get_query_var( 'cat' ) );
+	$published_posts = $cat->count
+	$args = array(
+		'posts_per_page'   => 4,
+		'offset'           => 0,
+		'orderby'          => 'date',
+		'order'            => 'DESC',
+		'category'          => $cat->cat_ID,
+		'post_type'        => 'post',
+		'post_status'      => 'publish',
+		'suppress_filters' => true 
+	);
+	$urlstart = get_template_directory_uri();
+	$posts = get_posts($args);
+	$i=0;
+	$exclude_ids = array();
+	foreach($posts as $post){
+		setup_postdata($post);
+		$categories = get_the_category();	
+		// add clearfix every 4 or 2 posts...
+		if($i%4 == 0){
+			echo '<div class="clearfix hidden-xs"></div>';
+		}elseif($i%2 == 0 ){
+			echo '<div class="clearfix visible-xs"></div>';
+		}
+		$i+=1;
+		$exclude_ids[]= $post->ID;
+		?>
 
-<header class="archive-header">
-<h1 class="archive-title"> <?php single_cat_title( '', true ); ?></h1>
-
-</header>
-
-<?php
-
-// The Loop
-while ( have_posts() ) : the_post(); ?>
-<div class="blog-cat-posts">
-	<div class="blog-cat-post-image">
-		<?php echo get_the_post_thumbnail($post->ID,"medium");?>
-	</div>
-	<div class="blog-cat-post-title">
-		<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
-			<?php the_title(); ?>
-		</a>
-	</div>
-	<div class="blog-cat-post-excerpt">
-	<?php the_excerpt(); ?>
-	</div>
-		<?php	$categories = get_the_category();
-			if ( ! empty( $categories ) ) { 
+		<div class="col-md-3 col-xs-6 text-center blog-other-teaser">
+			<a href="<?php echo get_permalink($post->ID); ?>" title="<?php get_the_title($post->ID); ?>">
+				<?php //see https://developer.wordpress.org/reference/functions/get_the_post_thumbnail/#comment-314 
+					if(has_post_thumbnail($post->ID)){
+						echo get_the_post_thumbnail($post->ID,'blog-thumbnail');
+					}else{
+						echo '<img src="'. $urlstart .'/images/blog-default-image.jpg" alt="">';
+					}
+				?></a>
+				<h4 class="blog-other-title">
+					<a href="<?php echo get_permalink($post->ID); ?>" title="<?php get_the_title($post->ID); ?>"><?php echo get_the_title( $post->ID );?></a>
+				</h4>
+				<p class="blog-other-excerpt">		
+					<?php echo wp_trim_words(get_the_content($post->ID), 20, '...' ); ?>
+				</p>
+			
+			<?php if ( ! empty( $categories ) ) {
 				get_category_button( $categories[0]->cat_ID);
-			}  ?>
-</div>
-<?php endwhile; 
-
-else: ?>
-<p>Sorry, no posts matched your criteria.</p>
-
-
-<?php endif; ?>
+			 	}  ?>
+			</div>
+		<?php wp_reset_postdata($post);
+		}
+		?>
+			</div>
+			<?php 
+			if($published_posts > sizeof($exclude_ids)){ 
+				$skip_posts = implode(", ",$exclude_ids);
+				$destroy = ceil(($published_posts - sizeof($exclude_ids)) / 4); 
+				?> 
+			<div class="row">
+				<div class="col-sm-12 text-center">
+			<?php echo do_shortcode('[ajax_load_more post_type="post" category="'.$cat->slug.'" exclude="'.$skip_posts.'" posts_per_page="4" pause="true" scroll="false" transition="none" images_loaded="true" destroy_after="'.$destroy.'" button_label="Load More" container_type="div"]'); ?>
+			</div>
+		</div>
+		<?php 
+		}	?>
+	</div>
 </div>
 </section>
 
