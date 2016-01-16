a73512c505c7b7ee306d6791cf22192ef5d2404a
Rhia Bucklin <rbucklin@uptopcorp.com>
2016-01-15 16:54:07 -0800

    design updates for the giving page

--- themes/the-dream-bed/page-giving.php
+++ themes/the-dream-bed/page-giving.php
@@ -4,78 +4,54 @@
 
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-2 col-sm-offset-1">
-				<img src="<?php bloginfo("template_url"); ?>/images/circle-dream-it-forward-buy-a-dream-give-a-dream.svg" width="238" height="238" alt="Dream It Forward - Buy a Dream, Give a Dream" class="img-responsive">
-			</div>
-			<div class="col-sm-8">
-				<h2>When you purchase The Dream Bed, we give a bed to a child or a person in need.</h2>
-				<p>A good night's sleep is the foundation for everything we do. It affects our health, happiness, success and ability to dream big. That's why we want to share the gift of sleep with everyone everywhere.</p>
-				<p>Your Dream Bed purchase makes a difference in a simple yet powerful way. With every bed purchased, we give a child or family in need an opportunity for a comfortable and healthy sleep space. We spend a third of our lives sleeping. Why not sleep on dreamy layers of luxurious comfort and donate the same to others? Unbox a better tomorrow through our Get One, Give One component and we will deliver a better night’s sleep to those in need.</p>
-				<p>We collaborate with numerous giving partners to determine where we can impact the dreams of others through products and services because everyone deserves to dream. Dream big and make a positive social impact to improve lives and fulfill the Get One, Give One promise around the country. Better sleep means bigger dreams for everyone!</p>
+			<div class="col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-3 text-center">
+				<h2>When you purchase The Dream Bed, we give a bed to someone in need.</h2>
+				<p>A good night&rsquo;s sleep is the foundation for everything we do. It affects our health, happiness, success and ability to dream big. That&rsquo;s why we want to share the gift of sleep with everyone everywhere.</p>
 			</div>
 		</div>
-		
-		<?php while ( have_posts() ) : the_post(); ?>
-
-				
-		<?php if (has_post_thumbnail( $post->ID ) ): ?>
-		<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-		<div class="row">
-			<div class="col-sm-6 col-sm-offset-3">
-  
-  				<img src="<?php echo $image[0]; ?>" class="img-responsive center-block">
-
-  			</div>
-
-			</div>
-		
-		<?php endif; ?>
-		
-		<div class="row">
-			<div class="col-sm-12 text-center">
-			
-				<?php the_content() ?>
-				
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				
-				
-			</div>
-		</div>
-		<?php endwhile; // End of the loop. ?>
+	</div>
 	
-</div>
+	<div class="light-grey-shapes giving-map">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1">
+					<div><img src="<?php bloginfo("template_url"); ?>/images/dream-bed-world-giving-map.png" class="img-responsive" alt="Why We Give" nopin="nopin"></div>
+					<img src="<?php bloginfo("template_url"); ?>/images/dream-bed-world-giving-map2.png" class="pin" alt="Why We Give" data-pin-description="The Dream Bed's goal is to provide those in need with a comfortable place to sleep.">
+				</div>
+			</div>
+		</div>
+	</div>
 
-<div class="light-grey-fill">
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 text-center">
-				<h2>How we dream</h2>
+				<h2>How it works</h2>
 			</div>
 		</div>
 		<div class="row how-we-dream">
 			<div class="col-sm-4 text-center">
-				<h2 class="callout-number">1</h2>
-				<h3>Buy a dream</h3>
-				<p>We believe access to a great night's sleep should be a universal right, and your purchase helps make it possible.</p>
-				<p>With the sale of each Dream Bed, you help provide dreams to children or persons in need through our buy one give one program.</p>
+				<img src="<?php bloginfo("template_url"); ?>/images/icon-buy.png" alt="" class="center-block">
+				<h4>1) You Shop</h4>
+				<p>You buy a Dream Bed. That&rsquo;s  it! With the sale of each Dream Bed you help provide dreams to people in need through our Buy One, Give One model.</p>
 			</div>
 			<div class="col-sm-4 text-center">
-				<h2 class="callout-number">2</h2>
-				<h3>Give a dream</h3>
-				<p>Your purchase makes a difference. Every Dream Bed purchased gives a person in need an opportunity for a clean, comfortable and  healthy sleep space.</p>
-				<p>Better sleep health means bigger dreams for everyone!</p>
+				<img src="<?php bloginfo("template_url"); ?>/images/icon-gift.png" alt="" class="center-block">
+				<h4>2) We Donate</h4>
+				<p>When you purchase a Dream Bed we give a bed to someone in need. Every Dream Bed sold provides someone an opportunity for a clean, comfortable and healthy sleep space.</p>
 			</div>
 			<div class="col-sm-4 text-center">
-				<h2 class="callout-number">3</h2>
-				<h3>Dream it forward</h3>
-				<p>Our Dream Bed Team collaborates with numerous giving partners to determine where we can impact the dreams of others through products and services.</p>
-				<p>Our strategy is simple: Buy a dream, give a dream, dream it forward!</p>
+				<img src="<?php bloginfo("template_url"); ?>/images/icon-hand.png" alt="" class="center-block">
+				<h4>3) Dream it forward</h4>
+				<p>Our Dream Bed Team collaborates with numerous giving partners to determine where we can impact the dreams of others through products and services. Our strategy is simple: Buy a dream, give a dream, dream it forward!</p>
 			</div>
 		</div>
-		
-	</div>
+	</div>	
 	
+	<div class="dark-grey-shapes">
+		<div>
+			<h2 class="callout-number">317</h2> beds donated to date
+		</div>
+	</div>
 	
 	
 
