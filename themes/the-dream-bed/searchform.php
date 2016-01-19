905633bac6dbdd2f92a4ee2dc5e9526b82cbb3b5
Ben Gourarie <bgourarie@uptopcorp.com>
2016-01-19 14:27:30 -0800

    i like parenthesis so much that i added two. Had to remove one so the page loads properly

--- themes/the-dream-bed/searchform.php
+++ themes/the-dream-bed/searchform.php
@@ -12,7 +12,7 @@
 		<?php if($is_blog){ ?>
 			<input type="hidden" value="post" name="post_type" id="post_type" />
 		<?php } ?>
-		<?php if($is_faq)){ ?>
+		<?php if($is_faq){ ?>
 			<input type="hidden" value="faq" name="post_type" id="post_type" />
 		<?php } ?>
 		<input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
