ac90a338c32802003dad067c5d2a504f3fbbbb63
Rhia Bucklin <rbucklin@uptopcorp.com>
2016-01-19 16:54:44 -0800

    seriously, like, collapse!

--- themes/the-dream-bed/page-faq.php
+++ themes/the-dream-bed/page-faq.php
@@ -53,7 +53,7 @@ foreach($categories as $category) {
 						$i=1;
 						while ($faq_prod_query->have_posts() ) {
 							$faq_prod_query->the_post();
-							echo '<div class="a-question"><h3><a href="#question'.$i.'" aria-expanded="true" aria-controls="question'.$i.'">'. get_the_title() . '</a></h3>
+							echo '<div class="a-question"><h3><a href="#question'.$i.'" aria-expanded="true" aria-controls="question'.$i.'" data-toggle="collapse">'. get_the_title() . '</a></h3>
 									<p id="question'.$i.'" class="collapse">' . get_the_content() .'</p></div>
 								';
 								$i+=1;
@@ -81,7 +81,7 @@ foreach($categories as $category) {
 				$i = 1;
 				while ($faq_query->have_posts() ) {
 					$faq_query->the_post();
-					echo '<div class="a-question"><h3><a href="#question'.$i.'" aria-expanded="true" aria-controls="question'.$i.'">'. get_the_title() . '</a></h3>
+					echo '<div class="a-question"><h3><a href="#question'.$i.'" aria-expanded="true" aria-controls="question'.$i.'" data-toggle="collapse">'. get_the_title() . '</a></h3>
 									<p id="question'.$i.'" class="collapse">' . get_the_content() .'</p></div>
 								';
 					$i+=1;
