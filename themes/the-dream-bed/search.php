b99f9084424021c8cccca98b53d7790a2d36229f
Rhia Bucklin <rbucklin@uptopcorp.com>
2016-01-20 16:50:19 -0800

    made the search results page look like something that belongs on this site

--- themes/the-dream-bed/search.php
+++ themes/the-dream-bed/search.php
@@ -9,33 +9,54 @@ get_header(); ?>
 
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			
+			<div class="container">	
+		
+			<?php if ( have_posts() ) : ?>
+			
+				<div class="row">
+			
+					<header class="page-header">
+						<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'the-dream-bed' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+					</header><!-- .page-header -->
+		
+					<?php /* Start the Loop */ ?>
+					
+					<?php while ( have_posts() ) : the_post(); ?>
+						<div class="search-result">
+						
+						
+						
+						<h3>
+							<a href="<?php echo get_permalink($post->ID); ?>" title="<?php get_the_title($post->ID); ?>">
+								<?php echo get_the_title(get_the_ID()); ?> 
+							</a>
+						</h3>
+						<p>
+							<?php echo wp_trim_words(get_the_content($post->ID), 80, '...' ); ?>
+						</p>
+						
+						<p>
+							<a href="<?php echo get_permalink($post->ID); ?>" title="<?php get_the_title($post->ID); ?>" class="btn btn-dream" role="button">
+								Read more
+							</a>
+						</p>
+						
+						</div>
+					<?php endwhile; ?>
+					
+					<?php the_posts_navigation(array('prev_text'=>"Older Results",'next_text'=>"Newer Results")); ?>
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'the-dream-bed' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<?php /* Start the Loop */ ?>
-			<ul>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<li>
-				<h3>
-					<a href="<?php echo get_permalink($post->ID); ?>" title="<?php get_the_title($post->ID); ?>">
-						<?php echo get_the_title(get_the_ID()); ?> 
-					</a>
-				</h3>
-					<?php echo wp_trim_words(get_the_content($post->ID), 20, '...' ); ?>
-				</li>
-			<?php endwhile; ?>
-			</ul>
-			<?php the_posts_navigation(array('prev_text'=>"Older Results",'next_text'=>"Newer Results")); ?>
+				</div>
 
 		<?php else : ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
+
+
+			</div>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
