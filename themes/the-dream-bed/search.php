8630c4382bfb1f26e41255e7385345cb4c0f0765
Rhia Bucklin <rbucklin@uptopcorp.com>
2016-01-20 17:02:52 -0800

    added some stuff for when there's no search results

--- themes/the-dream-bed/search.php
+++ themes/the-dream-bed/search.php
@@ -10,11 +10,12 @@ get_header(); ?>
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			
-			<div class="container">	
+			<div class="container search-page">	
 		
 			<?php if ( have_posts() ) : ?>
 			
 				<div class="row">
+					<div class="col-sm-12">
 			
 					<header class="page-header">
 						<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'the-dream-bed' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
@@ -46,16 +47,29 @@ get_header(); ?>
 					<?php endwhile; ?>
 					
 					<?php the_posts_navigation(array('prev_text'=>"Older Results",'next_text'=>"Newer Results")); ?>
-
+					
+					</div>
 				</div>
 
 		<?php else : ?>
+		
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<header class="page-header">
+						<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'the-dream-bed' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+					</header><!-- .page-header -->
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+						<p>Sorry, there are no search results for <?php printf('<b>' . get_search_query() . '</b>' ); ?> on the site. </p>
+						
+						<p>
+							<a href="javascript:history.back()" class="btn btn-dream" role="button">
+								<i class="fa fa-chevron-left"></i> Go Back
+							</a>
+						</p>
+						
+					<?php endif; ?>
 
-		<?php endif; ?>
-
-
+				</div>
 			</div>
 
 		</main><!-- #main -->
