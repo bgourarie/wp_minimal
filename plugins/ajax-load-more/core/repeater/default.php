e0866bd508dc7057e264b95c26821b52002869eb
Ben Gourarie <bgourarie@uptopcorp.com>
2016-03-21 15:14:24 -0700

    updating and cleaning up plugins

--- plugins/ajax-load-more/core/repeater/default.php
+++ plugins/ajax-load-more/core/repeater/default.php
@@ -1,11 +1,9 @@
-<?php 
-$categories = get_the_category();	
-$loadMoreCount+= 1; 
-output_post_teaser($post);
-if( $loadMoreCount % 4 == 0){
-	echo '<div class="clearfix hidden-xs"></div>';
-}if( $loadMoreCount % 2 == 0){
-	echo '<div class="clearfix visible-xs"></div>';
-}
-
-?>
+<li<?php if (! has_post_thumbnail() ) { echo ' class="no-img"'; } ?>>
+   <?php if ( has_post_thumbnail() ) { the_post_thumbnail(array(150,150));
+   }?>
+   <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+   <p class="entry-meta">
+       <?php the_time("F d, Y"); ?>
+   </p>
+   <?php the_excerpt(); ?>
+</li>
